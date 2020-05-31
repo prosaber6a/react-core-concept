da9ce3fb8405bf725ef0ee64d3929787488a8385
Saber Hossen Rabbani <saber.miazy@gmail.com>
2020-05-31 07:36:28 +0600

    Dynamic data pass to component

--- src/App.js
+++ src/App.js
@@ -8,23 +8,26 @@ function App() {
     <div className="App">
     <header className="App-header">
     <p>I am a React Person</p>
-    <Person></Person>
-    <Person></Person>
-    <Person></Person>
+    <Person name="Rubel" food="Fish"></Person>
+    <Person name="Sakil" food="Chicken"></Person>
+    <Person name="Zahid" food="Egg"></Person>
     </header>
     </div>
     );
   }
   
-  function Person() {
+  function Person(properties) {
+    console.log(properties);
+    
     const personStyle = {
       border:"2px solid red",
-      margin: "10px"
+      margin: "10px",
+      width: "400px"
     }
     return (
     <div style={personStyle}>
-      <h1>Name: Sakib Al Hasan</h1>
-      <h3>Hero of the year</h3>
+      <h1>Name: {properties.name}</h1>
+      <h3>Loves to eat {properties.food}</h3>
     </div>
     );
   }
